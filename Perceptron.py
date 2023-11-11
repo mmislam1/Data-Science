2295c154e688b499f1ed114177cb66185021fee2
Mohaiminul Islam <36233551+mmislam1@users.noreply.github.com>
2023-11-11 23:58:50 +0600

    Create Perceptron.py

---
+++ Perceptron.py
@@ -0,0 +1,18 @@
+# perceptor Practice  
+import matplotlib.pyplot as plt  
+import numpy as np  
+  
+inputs=np.array ([-1,-2,-3,-4])  
+weight=np.array ([.3,.4,-1.5,6])  
+  
+cal=inputs*weight  
+  
+print(cal)  
+  
+sums=np.sum(cal)  
+print('Summation:',sums)  
+  
+if sums>=1:  
+    print ('yes')  
+elif sums<=0:  
+    print ('Decision: ','No') 
